7fcbf1ee4e17a659cbec5edb19fcabdbae8f189b
Sergey Buksha <sergey066@yandex.ru>
2023-12-18 11:26:56 +0300

    App name added

--- app/src/main/java/ru/sb066coder/lemonade/MainActivity.kt
+++ app/src/main/java/ru/sb066coder/lemonade/MainActivity.kt
@@ -6,11 +6,15 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredHeight
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
@@ -26,6 +30,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -54,39 +60,52 @@ fun LemonadeApp() {
     var state by remember {
         mutableStateOf(State(0))
     }
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .wrapContentSize(),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Image(
-            painter = painterResource(id = state.imageResource),
-            modifier = Modifier
-                .padding(16.dp)
-                .clickable { state = state.change() }
-                .background(
-                    color = Color(0xFFC5F5D7),
-                    shape = RoundedCornerShape(32.dp)
-                )
-                .height(240.dp)
-                .aspectRatio(1.0f),
-            contentDescription = when(state.frame) {
-                1 -> stringResource(R.string.lemon_tree)
-                2 -> stringResource(R.string.lemon)
-                3 -> stringResource(R.string.glass_of_lemonade)
-                else -> stringResource(R.string.empty_glass)
-            }
-        )
+    Box {
         Text(
-            text = when(state.frame) {
-                1 -> stringResource(id = R.string.tap_the_lemon_tree)
-                2 -> stringResource(id = R.string.keep_tapping_the_lemon)
-                3 -> stringResource(id = R.string.tap_the_lemonade)
-                else -> stringResource(id = R.string.tap_the_empty_glass)
-            },
-            fontSize = 18.sp
+            modifier = Modifier
+                .fillMaxWidth()
+                .requiredHeight(80.dp)
+                .background(Color(0xFFFFEB3B))
+                .wrapContentHeight(),
+            textAlign = TextAlign.Center,
+            fontSize = 32.sp,
+            fontWeight = FontWeight.Bold,
+            text = stringResource(id = R.string.app_name)
         )
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .wrapContentSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(id = state.imageResource),
+                modifier = Modifier
+                    .padding(16.dp)
+                    .clickable { state = state.change() }
+                    .background(
+                        color = Color(0xFFC5F5D7),
+                        shape = RoundedCornerShape(32.dp)
+                    )
+                    .height(240.dp)
+                    .aspectRatio(1.0f),
+                contentDescription = when (state.frame) {
+                    1 -> stringResource(R.string.lemon_tree)
+                    2 -> stringResource(R.string.lemon)
+                    3 -> stringResource(R.string.glass_of_lemonade)
+                    else -> stringResource(R.string.empty_glass)
+                }
+            )
+            Text(
+                text = when (state.frame) {
+                    1 -> stringResource(id = R.string.tap_the_lemon_tree)
+                    2 -> stringResource(id = R.string.keep_tapping_the_lemon)
+                    3 -> stringResource(id = R.string.tap_the_lemonade)
+                    else -> stringResource(id = R.string.tap_the_empty_glass)
+                },
+                fontSize = 18.sp
+            )
+        }
     }
 }
 
